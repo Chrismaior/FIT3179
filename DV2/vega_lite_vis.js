76c21b9a744b638f85ea49547472f17ed30ed7af
Chrismaior <cmai0010@student.monash.edu>
2025-10-01 16:27:12 +1000

    added in treemap from external source - get it checked

--- DV2/vega_lite_vis.js
+++ DV2/vega_lite_vis.js
@@ -2,3 +2,5 @@ var vg_1 = "chloropleth.vg.json";
 vegaEmbed("#world_map", vg_1).then(function(result) {
 
 }).catch(console.error);
+
+vegaEmbed('#treemap', 'treeMap.vg.json', {actions:false}).catch(console.error);
