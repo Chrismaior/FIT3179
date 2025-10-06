637b055c8175e99c4721e52d4b07155490768275
Chrismaior <cmai0010@student.monash.edu>
2025-10-06 19:14:30 +1100

    added functionalities to line chart

--- DV2/vega_lite_vis.js
+++ DV2/vega_lite_vis.js
@@ -4,3 +4,8 @@ vegaEmbed("#world_map", vg_1).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed('#treemap', 'treeMap.vg.json', {actions:false}).catch(console.error);
+
+var vg_2 = "interactive_line_chart.vg.json";
+vegaEmbed("#homi_line_chart", vg_2).then(function(result) {
+
+}).catch(console.error);
