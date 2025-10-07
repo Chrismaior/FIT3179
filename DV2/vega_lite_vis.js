76703427bfb6623534e9a2869bde9b882bb1c407
Chrismaior <cmai0010@student.monash.edu>
2025-10-08 01:02:43 +1100

    added first donut chart

--- DV2/vega_lite_vis.js
+++ DV2/vega_lite_vis.js
@@ -9,3 +9,8 @@ var vg_2 = "interactive_line_chart.vg.json";
 vegaEmbed("#homi_line_chart", vg_2).then(function(result) {
 
 }).catch(console.error);
+
+var vg_3 = "drug_donut_chart.vg.json";
+vegaEmbed("#drug_donut", vg_3).then(function(result) {
+
+}).catch(console.error);
