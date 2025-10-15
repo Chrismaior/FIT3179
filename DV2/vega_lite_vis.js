95191a69f38164fcef1a35c1b12a3a6e4f01a9b0
Chrismaior <cmai0010@student.monash.edu>
2025-10-15 12:40:16 +1100

    added text and fixed sight lines

--- DV2/vega_lite_vis.js
+++ DV2/vega_lite_vis.js
@@ -1,41 +1,41 @@
 var vg_1 = "chloropleth.vg.json";
-vegaEmbed("#world_map", vg_1).then(function(result) {
+vegaEmbed("#world_map", vg_1, { actions: false }).then(function(result) {
 
 }).catch(console.error);
 
 vegaEmbed('#treemap', 'treeMap.vg.json', {actions:false}).catch(console.error);
 
 var vg_2 = "interactive_line_chart.vg.json";
-vegaEmbed("#homi_line_chart", vg_2).then(function(result) {
+vegaEmbed("#homi_line_chart", vg_2,{ actions: false }).then(function(result) {
 
 }).catch(console.error);
 
 var vg_3 = "drug_donut_chart.vg.json";
-vegaEmbed("#drug_donut", vg_3).then(function(result) {
+vegaEmbed("#drug_donut", vg_3,{ actions: false }).then(function(result) {
 
 }).catch(console.error);
 
 var vg_4 = "homicide_donut_chart.vg.json";
-vegaEmbed("#homicide_donut", vg_4).then(function(result) {
+vegaEmbed("#homicide_donut", vg_4,{ actions: false }).then(function(result) {
 
 }).catch(console.error);
 
 var vg_5 = "economic_donut_chart.vg.json";
-vegaEmbed("#economic_donut", vg_5).then(function(result) {
+vegaEmbed("#economic_donut", vg_5,{ actions: false }).then(function(result) {
 
 }).catch(console.error);
 
 var vg_6 = "violence_donut_chart.vg.json";
-vegaEmbed("#violence_donut", vg_6).then(function(result) {
+vegaEmbed("#violence_donut", vg_6,{ actions: false }).then(function(result) {
 
 }).catch(console.error);
 
 var vg_7 = "boxplot.vg.json";
-vegaEmbed("#boxplot", vg_7).then(function(result) {
+vegaEmbed("#boxplot", vg_7,{ actions: false }).then(function(result) {
 
 }).catch(console.error);
 
 var vg_8 = "resilience_avg.vg.json";
-vegaEmbed("#resilience_bar", vg_8).then(function(result) {
+vegaEmbed("#resilience_bar", vg_8,{ actions: false }).then(function(result) {
 
 }).catch(console.error);
